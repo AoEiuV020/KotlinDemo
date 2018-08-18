40619fb370a79c0d3a1000ceeb517da0b0527c81
AoEiuV020 <aoeiuv020@gmail.com>
2018-08-18 10:26:53 +0800

    show baidu poi address

--- app/src/main/java/cc/aoeiuv020/demo/MainActivity.java
+++ app/src/main/java/cc/aoeiuv020/demo/MainActivity.java
@@ -2,7 +2,9 @@ package cc.aoeiuv020.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -11,12 +13,24 @@ import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MainActivity";
 
     private TextView mLocationTextView;
     private LocationClient mLocationClient;                 // 声明LocationClient类
+    private double mLongitude;
+    private double mLatitude;
     private BDAbstractLocationListener mMyLocationListener = new BDAbstractLocationListener() {
 
         @Override
@@ -35,8 +49,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             // 百度定位成功
             mLocationClient.stop();
-            double mLongitude = location.getLongitude();
-            double mLatitude = location.getLatitude();
+            mLongitude = location.getLongitude();
+            mLatitude = location.getLatitude();
             String mAddress = location.getAddrStr();
             String mProvinceName = location.getProvince();
             String mCityName = location.getCity();
@@ -60,6 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.select_btn).setOnClickListener(this);
         findViewById(R.id.map_btn).setOnClickListener(this);
         findViewById(R.id.image_btn).setOnClickListener(this);
+        findViewById(R.id.poi_btn).setOnClickListener(this);
         mLocationTextView = findViewById(R.id.location_tv);
         mLocationClient = new LocationClient(this);
         mLocationClient.registerLocationListener(mMyLocationListener); // 注册监听函数
@@ -84,6 +99,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.image_btn:
+                break;
+            case R.id.poi_btn:
+                GeoCoder geoSearch = GeoCoder.newInstance();
+                geoSearch.setOnGetGeoCodeResultListener(new OnGetGeoCoderResultListener() {
+                    /**
+                     * 地理编码，由地点名称获取经纬度
+                     */
+                    @Override
+                    public void onGetGeoCodeResult(GeoCodeResult result) {
+                    }
+
+                    /**
+                     * 反地理编码，由地点经纬度获取地点名称
+                     */
+                    @Override
+                    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+                        List<PoiInfo> poiList = result.getPoiList();
+                        List<String> addList = new ArrayList<>(poiList.size());
+                        for (PoiInfo poi :
+                                poiList) {
+                            addList.add(poi.address);
+                        }
+                        String name = result.getBusinessCircle();
+                        if (TextUtils.isEmpty(name)) {
+                            name = result.getSematicDescription();
+                        }
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle(name)
+                                .setItems(addList.toArray(new String[0]), null)
+                                .show();
+                    }
+                });
+                /* 加载定位数据 */
+                LatLng latLng = new LatLng(mLatitude, mLongitude);
+                ReverseGeoCodeOption reverseGeoCodeOption = new ReverseGeoCodeOption();
+                reverseGeoCodeOption.location(latLng);
+                geoSearch.reverseGeoCode(reverseGeoCodeOption);
+
                 break;
         }
     }
