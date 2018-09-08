a373cff203cf55f58cb5840e142b94136ae00b35
AoEiuV020 <aoeiuv020@gmail.com>
2018-09-09 02:18:27 +0800

    app activity use ssl

--- app/src/main/java/cc/aoeiuv020/demo/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/demo/MainActivity.kt
@@ -2,14 +2,34 @@ package cc.aoeiuv020.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import cc.aoeiuv020.ssl.TLSSocketFactory
+import cc.aoeiuv020.ssl.TrustManagerUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.CipherSuite
+import okhttp3.ConnectionSpec
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import java.security.cert.X509Certificate
 
 class MainActivity : AppCompatActivity() {
+    private val spec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+            .cipherSuites(
+                    CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+                    *ConnectionSpec.MODERN_TLS.cipherSuites()!!.toTypedArray()
+            )
+            .build()
+
+    private fun OkHttpClient.Builder.sslInclude(vararg certList: X509Certificate) = apply {
+        TrustManagerUtils.include(certList.toSet()).let { tm ->
+            sslSocketFactory((TLSSocketFactory(tm)), tm)
+        }
+    }
+
     private val client = OkHttpClient.Builder()
+            .connectionSpecs(listOf(spec, ConnectionSpec.CLEARTEXT))
+            .sslInclude()
             .build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
