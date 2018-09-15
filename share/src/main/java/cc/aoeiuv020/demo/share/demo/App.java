766b31531dc93df03309948bfd90fbe14300e644
AoEiuV020 <aoeiuv020@gmail.com>
2018-09-15 11:27:03 +0800

    create share application
    and change application id,

---
+++ share/src/main/java/cc/aoeiuv020/demo/share/demo/App.java
@@ -0,0 +1,6 @@
+package cc.aoeiuv020.demo.share.demo;
+
+import android.support.multidex.MultiDexApplication;
+
+public class App extends MultiDexApplication {
+}
---
+++ share/src/main/java/cc/aoeiuv020/demo/share/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package cc.aoeiuv020.demo.share.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
