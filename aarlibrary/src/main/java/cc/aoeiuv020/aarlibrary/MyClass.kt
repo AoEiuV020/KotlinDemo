2bb40d322ad1a6d2ae8d24e7ab198af714827d91
AoEiuV020 <aoeiuv020@gmail.com>
2018-11-21 09:54:58 +0800

    test doc link

--- aarlibrary/src/main/java/cc/aoeiuv020/aarlibrary/MyClass.kt
+++ aarlibrary/src/main/java/cc/aoeiuv020/aarlibrary/MyClass.kt
@@ -1,6 +1,14 @@
 package cc.aoeiuv020.aarlibrary
 
+import android.content.Context
+
 /**
  * 测试dokka,
  */
-class MyClass
+class MyClass {
+    /**
+     * 测试dokka, [Context]
+     * @see Context
+     */
+    lateinit var ctx: Context
+}
--- jarlibrary/src/main/java/cc/aoeiuv020/jarlibrary/MyClass.kt
+++ jarlibrary/src/main/java/cc/aoeiuv020/jarlibrary/MyClass.kt
@@ -4,4 +4,10 @@ package cc.aoeiuv020.jarlibrary
  * 测试dokka,
  *
  */
-class MyClass
+class MyClass {
+    /**
+     * 测试dokka, [String]
+     * @see String
+     */
+    lateinit var str: String
+}
