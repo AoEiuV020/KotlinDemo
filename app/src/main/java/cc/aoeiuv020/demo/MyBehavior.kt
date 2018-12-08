644cd12f684ea819c78381c8f448ddecf7cad1e6
AoEiuV020 <aoeiuv020@gmail.com>
2018-12-08 12:10:22 +0800

    改用ScrollView

---
+++ app/src/main/java/cc/aoeiuv020/demo/MyBehavior.kt
@@ -0,0 +1,13 @@
+package cc.aoeiuv020.demo
+
+import android.content.Context
+import android.support.design.widget.CoordinatorLayout
+import android.util.AttributeSet
+import android.view.View
+
+class MyBehavior : CoordinatorLayout.Behavior<View> {
+    constructor() : super()
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+
+}
---
+++ app/src/main/java/cc/aoeiuv020/demo/MyScrollView.kt
@@ -0,0 +1,20 @@
+package cc.aoeiuv020.demo
+
+import android.content.Context
+import android.support.v4.widget.NestedScrollView
+import android.util.AttributeSet
+
+class MyScrollView : NestedScrollView {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+    override fun dispatchNestedScroll(dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, offsetInWindow: IntArray?): Boolean {
+        return super.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow)
+    }
+
+    override fun dispatchNestedScroll(dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, offsetInWindow: IntArray?, type: Int): Boolean {
+        return super.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type)
+    }
+}
