e2dadbe082dac6343b28a60ac67658f9576fffd2
AoEiuV020 <aoeiuv020@gmail.com>
2019-05-01 12:59:38 +0800

    在application中监听activity创建设置主题

--- app/src/main/java/cc/aoeiuv020/App.kt
+++ app/src/main/java/cc/aoeiuv020/App.kt
@@ -1,5 +1,42 @@
 package cc.aoeiuv020
 
+import android.app.Activity
+import android.os.Bundle
 import androidx.multidex.MultiDexApplication
+import cc.aoeiuv020.anull.notNull
 
-class App : MultiDexApplication()
+class App : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity?) {
+
+            }
+
+            override fun onActivityResumed(activity: Activity?) {
+
+            }
+
+            override fun onActivityStarted(activity: Activity?) {
+
+            }
+
+            override fun onActivityDestroyed(activity: Activity?) {
+
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
+
+            }
+
+            override fun onActivityStopped(activity: Activity?) {
+
+            }
+
+            override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
+                activity.notNull().setTheme(R.style.Black)
+            }
+        })
+    }
+}
--- app/src/main/java/cc/aoeiuv020/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/MainActivity.kt
@@ -1,7 +1,5 @@
 package cc.aoeiuv020
 
-import android.content.Context
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,37 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    private var oResources: SkinResource? = null
-
-    override fun getResources(): Resources {
-        return oResources ?: SkinResource(
-                this,
-                super.getResources()
-        ).also { oResources = it }
-    }
-
-    class SkinResource(
-            ctx: Context,
-            resources: Resources
-    ) : Resources(
-            ctx.assets,
-            resources.displayMetrics,
-            resources.configuration
-    ) {
-
-        override fun getColor(id: Int, theme: Theme?): Int {
-            return when (id) {
-                R.color.colorPrimaryDark -> 0xff00ff00.toInt()
-                R.color.colorPrimary -> 0xff00ff00.toInt()
-                R.color.colorAccent -> 0xff00ff00.toInt()
-                else -> super.getColor(id, theme)
-            }
-        }
-
-        override fun getColor(id: Int): Int {
-            return getColor(id, null)
-        }
     }
 }
