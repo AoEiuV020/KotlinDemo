551b180b249c187e612efcbf6b83ce5ea9383c6e
AoEiuV020 <aoeiuv020@gmail.com>
2019-06-12 16:23:23 +0800

    动态申请sip权限

--- app/src/main/java/cc/aoeiuv020/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/MainActivity.kt
@@ -1,7 +1,9 @@
 package cc.aoeiuv020
 
+import android.Manifest.permission.USE_SIP
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import cc.aoeiuv020.sip.SipCallActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ActivityCompat.requestPermissions(this, arrayOf(
+                USE_SIP
+        ), 1)
 
         btnSip.setOnClickListener {
             SipCallActivity.start(this)
