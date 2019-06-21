1f9610ef7fa731273fd17865dfc8505536f3521c
AoEiuV020 <aoeiuv020@gmail.com>
2019-06-21 11:55:05 +0800

    监听键盘弹出收起打日志看高度

--- app/src/main/java/cc/aoeiuv020/KeyboardHeightActivity.kt
+++ app/src/main/java/cc/aoeiuv020/KeyboardHeightActivity.kt
@@ -3,9 +3,11 @@ package cc.aoeiuv020
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivity
 
-class KeyboardHeightActivity : AppCompatActivity() {
+class KeyboardHeightActivity : AppCompatActivity(), AnkoLogger {
     companion object {
         fun start(ctx: Context) {
             ctx.startActivity<KeyboardHeightActivity>()
@@ -16,5 +18,15 @@ class KeyboardHeightActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_keyboard_height)
+
+        SoftKeyBoardListener.setListener(this, object : SoftKeyBoardListener.OnSoftKeyBoardChangeListener {
+            override fun keyBoardShow(height: Int) {
+                info { "show $height" }
+            }
+
+            override fun keyBoardHide(height: Int) {
+                info { "hide $height" }
+            }
+        })
     }
 }
