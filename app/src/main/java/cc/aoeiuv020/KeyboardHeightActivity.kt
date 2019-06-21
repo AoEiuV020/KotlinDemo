cb8395774b0fdd1681d8c18100573f1f8bc78924
AoEiuV020 <aoeiuv020@gmail.com>
2019-06-21 12:02:49 +0800

    按钮实现底部占位视图设置高度和键盘一致

--- app/src/main/java/cc/aoeiuv020/KeyboardHeightActivity.kt
+++ app/src/main/java/cc/aoeiuv020/KeyboardHeightActivity.kt
@@ -2,7 +2,9 @@ package cc.aoeiuv020
 
 import android.content.Context
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_keyboard_height.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivity
@@ -12,8 +14,9 @@ class KeyboardHeightActivity : AppCompatActivity(), AnkoLogger {
         fun start(ctx: Context) {
             ctx.startActivity<KeyboardHeightActivity>()
         }
-
     }
+
+    private var mKeyboardHeight = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,11 +25,36 @@ class KeyboardHeightActivity : AppCompatActivity(), AnkoLogger {
         SoftKeyBoardListener.setListener(this, object : SoftKeyBoardListener.OnSoftKeyBoardChangeListener {
             override fun keyBoardShow(height: Int) {
                 info { "show $height" }
+                mKeyboardHeight = height
             }
 
             override fun keyBoardHide(height: Int) {
                 info { "hide $height" }
+                mKeyboardHeight = height
             }
         })
+
+        btnToggleView.setOnClickListener {
+            if (vBottom.visibility == View.GONE) {
+                showBottomView()
+            } else {
+                hideBottomView()
+            }
+        }
+    }
+
+    private fun hideBottomView() {
+        vBottom.visibility = View.GONE
+    }
+
+    private fun showBottomView() {
+        vBottom.visibility = View.VISIBLE
+        val height = mKeyboardHeight.takeIf { it > 0 }
+                ?: 200
+        if (vBottom.height != height) {
+            vBottom.layoutParams = vBottom.layoutParams.also {
+                it.height = height
+            }
+        }
     }
 }
