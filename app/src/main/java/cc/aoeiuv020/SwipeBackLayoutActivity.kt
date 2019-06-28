84a661d8e0ea9d851cdb54d650546ee2e4321645
AoEiuV020 <aoeiuv020@gmail.com>
2019-06-28 14:22:05 +0800

    测试生命周期影响

--- app/src/main/java/cc/aoeiuv020/SwipeBackLayoutActivity.kt
+++ app/src/main/java/cc/aoeiuv020/SwipeBackLayoutActivity.kt
@@ -2,12 +2,17 @@ package cc.aoeiuv020
 
 import android.content.Context
 import android.os.Bundle
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import kotlinx.android.synthetic.main.activity_swipe_back_layout.*
 import me.imid.swipebacklayout.lib.SwipeBackLayout
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivity
 
-class SwipeBackLayoutActivity : SwipeBackActivity() {
+class SwipeBackLayoutActivity : SwipeBackActivity(), AnkoLogger {
     companion object {
         fun start(ctx: Context, number: Int) {
             ctx.startActivity<SwipeBackLayoutActivity>("number" to number)
@@ -20,6 +25,13 @@ class SwipeBackLayoutActivity : SwipeBackActivity() {
 
         val number = intent.getIntExtra("number", 0)
         title = number.toString()
+
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+            fun onAny() {
+                info { "$title-${lifecycle.currentState}" }
+            }
+        })
 
         btnNewPage.setOnClickListener {
             start(this, number + 1)
