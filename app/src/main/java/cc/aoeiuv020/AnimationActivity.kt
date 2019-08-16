6e299877c142eaa2e17817d862f38bd00354b573
AoEiuV020 <aoeiuv020@gmail.com>
2019-08-16 11:52:40 +0800

    测试圆环扩散动画

--- app/src/main/java/cc/aoeiuv020/AnimationActivity.kt
+++ app/src/main/java/cc/aoeiuv020/AnimationActivity.kt
@@ -1,5 +1,6 @@
 package cc.aoeiuv020
 
+import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
@@ -19,21 +20,27 @@ class AnimationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
 
-        val animator = ValueAnimator.ofInt(80, 160).apply {
+
+        val animSet = AnimatorSet().apply {
             duration = 1000
-            repeatCount = 1
-            repeatMode = ValueAnimator.REVERSE
-            addUpdateListener {
-                val v = it.animatedValue as Int
-                ivCircle.layoutParams = ivCircle.layoutParams.apply {
-                    height = dip(v)
-                    width = dip(v)
+            play(ValueAnimator.ofInt(80, 240).apply {
+                addUpdateListener {
+                    val v = it.animatedValue as Int
+                    ivCircle.layoutParams = ivCircle.layoutParams.apply {
+                        height = dip(v)
+                        width = dip(v)
+                    }
                 }
-            }
+            }).with(ValueAnimator.ofFloat(1f, 0f).apply {
+                addUpdateListener {
+                    val v = it.animatedValue as Float
+                    ivCircle.alpha = v
+                }
+            })
         }
 
         ivCircle.setOnClickListener {
-            animator.start()
+            animSet.start()
         }
     }
 }
