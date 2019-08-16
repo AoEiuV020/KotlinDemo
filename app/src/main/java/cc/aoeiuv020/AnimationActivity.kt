1499b6da49b32e5e4b0f9f505ff706bd1b787771
AoEiuV020 <aoeiuv020@gmail.com>
2019-08-16 11:41:39 +0800

    测试放大动画

--- app/src/main/java/cc/aoeiuv020/AnimationActivity.kt
+++ app/src/main/java/cc/aoeiuv020/AnimationActivity.kt
@@ -1,10 +1,11 @@
 package cc.aoeiuv020
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.os.Bundle
-import android.view.animation.AlphaAnimation
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_animation.*
+import org.jetbrains.anko.dip
 import org.jetbrains.anko.startActivity
 
 class AnimationActivity : AppCompatActivity() {
@@ -18,10 +19,21 @@ class AnimationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation)
 
-        val anim = AlphaAnimation(1f, 0f)
-        anim.duration = 1000
+        val animator = ValueAnimator.ofInt(80, 160).apply {
+            duration = 1000
+            repeatCount = 1
+            repeatMode = ValueAnimator.REVERSE
+            addUpdateListener {
+                val v = it.animatedValue as Int
+                ivCircle.layoutParams = ivCircle.layoutParams.apply {
+                    height = dip(v)
+                    width = dip(v)
+                }
+            }
+        }
+
         ivCircle.setOnClickListener {
-            ivCircle.startAnimation(anim)
+            animator.start()
         }
     }
 }
