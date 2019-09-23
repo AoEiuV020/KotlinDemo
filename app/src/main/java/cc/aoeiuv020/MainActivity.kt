f6b5715668ee79e108df23e790ea0b8afab46ed0
AoEiuV020 <aoeiuv020@gmail.com>
2019-09-23 10:25:36 +0800

    测试TextView自带padding
    vivo x5m有问题，

--- app/src/main/java/cc/aoeiuv020/MainActivity.kt
+++ app/src/main/java/cc/aoeiuv020/MainActivity.kt
@@ -3,9 +3,7 @@ package cc.aoeiuv020
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
     companion object {
@@ -18,9 +16,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        btnHello.setOnClickListener {
-            toast("Hello")
-        }
     }
 }
