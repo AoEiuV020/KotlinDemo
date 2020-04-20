af727ead7a0d3b2a4ea899bef0350425d56e64c0
AoEiuV020 <aoeiuv020@gmail.com>
2020-04-20 16:21:13 +0800

    实现保存图片到相册

--- app/src/main/java/cc/aoeiuv020/ImageActivity.kt
+++ app/src/main/java/cc/aoeiuv020/ImageActivity.kt
@@ -1,16 +1,21 @@
 package cc.aoeiuv020
 
+import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import cc.aoeiuv020.anull.notNull
 import kotlinx.android.synthetic.main.activity_image.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
+import java.io.OutputStream
 import java.net.URL
+
 
 class ImageActivity : AppCompatActivity() {
     companion object {
@@ -19,6 +24,8 @@ class ImageActivity : AppCompatActivity() {
             ctx.startActivity<ImageActivity>()
         }
     }
+
+    private val imageName = "imageAndroidQTest.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +36,20 @@ class ImageActivity : AppCompatActivity() {
         btnDownload.setOnClickListener {
             doAsync {
                 URL(getUrl()).openStream().use { input ->
-                    openFileOutput("imageCache", Context.MODE_PRIVATE).use { output ->
+                    openFileOutput(imageName, Context.MODE_PRIVATE).use { output ->
                         input.copyTo(output)
                     }
                 }
                 uiThread {
-                    ivImage.setImageURI(Uri.fromFile(getFileStreamPath("imageCache")))
+                    ivImage.setImageURI(Uri.fromFile(getFileStreamPath(imageName)))
+                }
+            }
+        }
+
+        btnSave.setOnClickListener {
+            addPictureToAlbum(this, imageName) { output ->
+                openFileInput(imageName).use { input ->
+                    input.copyTo(output)
                 }
             }
         }
@@ -44,4 +59,13 @@ class ImageActivity : AppCompatActivity() {
         return etUrl.text.toString()
     }
 
+    @Suppress("SameParameterValue")
+    private fun addPictureToAlbum(context: Context, fileName: String, writer: (OutputStream) -> Unit) {
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, fileName)
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        val uri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues).notNull()
+        context.contentResolver.openOutputStream(uri).notNull().use(writer)
+    }
 }
