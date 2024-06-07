f4a7dc1c65a4132494acc9775216f232306cdc03
AoEiuV020 <aoeiuv020@gmail.com>
2024-06-08 00:47:37 +0800

    添加安卓库模块

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":sdk:androidLibrary"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "KotlinDemo"
 include(":app")
+include(":sdk:androidLibrary")
