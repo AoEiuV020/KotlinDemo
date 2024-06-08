8a66fca59242721a3ccf748b195ec384e413046f
AoEiuV020 <aoeiuv020@gmail.com>
2024-06-08 10:17:16 +0800

    Rename .java to .kt

---
+++ app/src/test/java/com/aoeiuv020/kotlindemo/AndroidLibraryClassTest.kt
@@ -0,0 +1,19 @@
+package com.aoeiuv020.kotlindemo
+
+import com.aoeiuv020.androidlibrary.AndroidLibraryClass
+import org.junit.Test
+import org.slf4j.simple.SimpleLogger
+
+class AndroidLibraryClassTest {
+    init {
+        System.setProperty(SimpleLogger.LOG_KEY_PREFIX + "JavaLibrary", "trace")
+        System.setProperty(SimpleLogger.LOG_KEY_PREFIX + "AndroidLibrary", "trace")
+    }
+    @Test
+    fun init() {
+        val androidLibraryClass = AndroidLibraryClass()
+        androidLibraryClass.init()
+        println(AndroidLibraryClass.androidLibraryBean)
+        println(AndroidLibraryClass.javaLibraryBean)
+    }
+}
