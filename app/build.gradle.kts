8e017ffaf488901d4d23dcbea210d96e66420a4e
AoEiuV020 <aoeiuv020@gmail.com>
2024-06-11 20:40:16 +0800

    slf4j具体实现改用runtimeOnly依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation(project(":sdk:androidlibrary"))
     implementation(platform(libs.slf4j.bom))
     implementation(libs.slf4j)
-    implementation(libs.slf4j.android)
+    runtimeOnly(libs.slf4j.android)
 
     testImplementation(libs.slf4j.simple)
 
