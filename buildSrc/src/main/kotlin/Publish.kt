20551deb5e943ee8e23bb120476415cfbd1bb22c
AoEiuV020 <aoeiuv020@gmail.com>
2024-12-31 00:30:59 +0800

    更新版本2.1.0

--- buildSrc/src/main/kotlin/Publish.kt
+++ buildSrc/src/main/kotlin/Publish.kt
@@ -1,6 +1,6 @@
 object Publish {
     const val groupId = "com.aoeiuv020"
-    const val version = "2.0.0"
+    const val version = "2.1.0"
     const val publishSourcesJar = true
 
     fun getArtifactId(path: String): String {
